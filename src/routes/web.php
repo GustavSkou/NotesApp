ed4411d749fb04c84c5ca8d55bba7cab9fcade75
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2025-10-26 18:44:56 +0100

    refactor

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 use App\Http\Controllers\NoteController;
 
 Route::get('/', function () {
@@ -12,6 +11,6 @@ Route::get('/dashboard', [NoteController::class, 'index'])->name('notes.index');
 Route::get('/create', [NoteController::class, 'create'])->name('notes.create');
 Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
 
-Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
+Route::post('/create', [NoteController::class, 'store'])->name('notes.store');
 
 Route::patch('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
