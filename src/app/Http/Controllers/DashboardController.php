fffb804a811bf56445df437b3e9ce892721967b3
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2025-10-29 14:08:38 +0100

    controller for new dashboard

---
+++ src/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function show()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function update(Request $request)
+    {
+    }
+}
---
+++ src/app/Http/Controllers/NoteBookController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\NoteBook;
+
+class NoteBookController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function show()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function update(Request $request)
+    {
+    }
+}
