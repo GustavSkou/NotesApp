2aacbbacdb19d2eb95591830125dfcbae4ebb850
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2025-11-05 17:39:16 +0100

    added middelware

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,19 +12,14 @@ Route::get('/', function () {
 
 Route::get('/login', [AuthController::class, 'ShowLogin'])->name('show.login');
 Route::get('/register', [AuthController::class, 'ShowRegister'])->name('show.register');
-
 Route::post('/register', [AuthController::class, 'Register'])->name('register');
 Route::post('/login', [AuthController::class, 'Login'])->name('login');
 
-Route::post('/logout', [AuthController::class, 'LogOut'])->name('logout');
-
-Route::get('/notebooks', [NoteBookController::class, 'index'])->name('notebook.index');             //show all notebook
-Route::get('/notebooks/{notebook}/chapters', [ChapterController::class, 'index'])->name('chapter.index');    //show all chapter under notebook
-Route::get('/notebooks/{notebook}/chapters/{chapter}/notes', [NoteController::class, 'index'])->name('note.index');            //show all notes under chapter
-Route::get('/notebooks/{notebook}/chapters/{chapter}/notes/{note}', [NoteController::class, 'show'])->name('notes.show');                   //show single note under notes
-
-
-
-Route::get('/create', [NoteController::class, 'create'])->name('notes.create');
-Route::post('/create', [NoteController::class, 'store'])->name('notes.store');
-Route::patch('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
+Route::post('/logout', [AuthController::class, 'LogOut'])->name('logout')->middleware('auth');
+Route::get('/notebooks', [NoteBookController::class, 'index'])->name('notebook.index')->middleware('auth');                                     //show all notebook
+Route::get('/notebooks/{notebook}/chapters', [ChapterController::class, 'index'])->name('chapter.index')->middleware('auth');                   //show all chapter under notebook
+Route::get('/notebooks/{notebook}/chapters/{chapter}/notes', [NoteController::class, 'index'])->name('note.index')->middleware('auth');         //show all notes under chapter
+Route::get('/notebooks/{notebook}/chapters/{chapter}/notes/{note}', [NoteController::class, 'show'])->name('notes.show')->middleware('auth');   //show single note under notes
+Route::get('/create', [NoteController::class, 'create'])->name('notes.create')->middleware('auth');
+Route::post('/create', [NoteController::class, 'store'])->name('notes.store')->middleware('auth');
+Route::patch('/notes/{note}', [NoteController::class, 'update'])->name('notes.update')->middleware('auth');
